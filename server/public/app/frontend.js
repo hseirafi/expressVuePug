7c77b9d7d2af667b8660f20dc0c68d8d9ad6e98d
hasan <hseirafi@gmail.com>
2017-03-07 14:08:17 -0800

    only local listen

--- server/public/app/frontend.js
+++ server/public/app/frontend.js
@@ -1,15 +1,1 @@
-webpackJsonp([1], {
-	262: function (n, o, e) {
-		"use strict";
-		function t(n) {
-			return n && n.__esModule ? n : {default: n}
-		}
-
-		var u = e(350);
-		t(u);
-		console.log("Frontend loaded!")
-	}, 350: function (n, o) {
-	}, 576: function (n, o, e) {
-		n.exports = e(262)
-	}
-}, [576]);
+webpackJsonp([1],{262:function(n,o,e){"use strict";function t(n){return n&&n.__esModule?n:{default:n}}var u=e(352);t(u);console.log("Frontend loaded!")},352:function(n,o){},586:function(n,o,e){n.exports=e(262)}},[586]);
