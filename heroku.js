a82bc8e1fedb0153e8dd370ce9e26169d3b7fcf0
hasan <hseirafi@gmail.com>
2017-03-17 23:21:14 -0700

    after build movie files from root to heroku directory

--- heroku.js
+++ heroku.js
@@ -2,12 +2,18 @@
 	* Created by hoss on 3/12/2017.
 	*/
 
-const fs = require("fs");
+const fs = require("fs-extra");
 const Task = require("data.task");
+const path = require("path");
 
+const copyPaste = (filename, destination) =>
+	new Task((res, rej) =>
+		fs.copy(filename, destination, (err, contents) =>
+			err ? rej(err) : res(contents)));
 
 const makeDir = (filename) =>
 	new Task((rej, res) =>
+		// returns undefined
 		fs.mkdirSync(filename, (err, contents) =>
 			err ? rej(err) : res(contents)));
 
@@ -22,60 +28,40 @@ const writeFile = (filename, enc) =>
 			err ? rej(err) : res(success)));
 
 
-// fs.mkdirSync(__dirname + '/heroku', err => err);
-// fs.mkdirSync(__dirname + '/heroku/server', err=> err);
+fs.mkdirSync(__dirname + "/heroku", err => err);
+fs.mkdirSync(__dirname + "/heroku/server", err => err);
 
-const mapObj={
-	'process.env.MONGO_USERNAME':'process.env.MONGODB_USERNAME',
-	'process.env.MONGO_PASSWORD':'process.env.MONGODB_PASSWORD',
-	'db: {':`db: {
+const strReplaceObj = {
+	"process.env.MONGO_USERNAME": "process.env.MONGODB_USERNAME",
+	"process.env.MONGO_PASSWORD": "process.env.MONGODB_PASSWORD",
+	"db: {": `db: {
 		uri: process.env.MONGODB_URI || "mongodb://localhost/vemapp",`
 };
 
-const re = new RegExp(Object.keys(mapObj).join("|"),"g");
+const regInit = new RegExp(Object.keys(strReplaceObj).join("|"), "g");
+const dirCopy = {
+	"server/locales": "heroku/server/locales",
+	"server/public":	"heroku/server/public",
+	"server/views": "heroku/server/views",
+	"server/bundle.js": "heroku/server/bundle.js"
+}
 
-console.log(mapObj['db: {']);
+const locations = Object.entries(dirCopy);
+
 const app =
-readFile("config.js", "utf-8")
-		.map(contents => contents.replace(re,matched=> mapObj[matched]))
+	readFile("config.js", "utf-8")
+		.map(contents => contents.replace(regInit, matched => strReplaceObj[matched]))
 		.chain(contents => writeFile("heroku/config.js", contents))
 		.chain(contents => readFile("config.js", "utf-8"))
 		.map(contents => contents.replace("4", "9"))
 		.chain(contents => writeFile("config2.js", contents))
+		.chain(contents => locations.forEach(([origin,base])=> copyPaste(origin, base)))
 
 
 app.fork(e => console.log(e),
-	x => console.log('success'))
+	x => console.log("success"));
 
 
-// const isThere = x =>({x,concat:({x:y})=>isThere(x && y),inspect:()=>`isThere(${x})`})
 
 
-// const some = isThere(true).concat(isThere(fs.existsSync('heroku') ));
 
-//const directory =xs => xs.reduce((acc,x)=> fs.existsSync(acc) && x,true);
-//const append = xs => xs.reduce((acc, x)=> acc + fs.mkdirSync(x),'');
-
-
-//console.log(result);
-
-
-// ar.reduce((a,b)=>{
-//
-// if(!fs.)
-//
-//  a+b,{};
-//
-//
-// }
-
-
-// );
-
-//console.log(some);
-
-
-// if(!fs.existsSync(heroku)){
-// 	fs.mkdirSync(heroku);
-//
-// }
