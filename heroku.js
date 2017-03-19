5af81cc8c3da84c4259b6a35e377fff6b53873e8
hasan <hseirafi@gmail.com>
2017-03-19 01:32:41 -0700

    un nesseary error handle on fs.mkdir

--- heroku.js
+++ heroku.js
@@ -6,14 +6,9 @@ const fs = require("fs-extra");
 const Task = require("data.task");
 const path = require("path");
 
-const copyPaste = (filename, destination) =>
-	new Task((res, rej) =>
-		fs.copy(filename, destination, (err, contents) =>
-			err ? rej(err) : res(contents)));
-
 const makeDir = (filename) =>
 	new Task((rej, res) =>
-		// returns undefined
+	
 		fs.mkdir(filename, (err, contents) =>
 			err ? rej(err) : res(contents)));
 
@@ -28,7 +23,6 @@ const writeFile = (filename, enc) =>
 			err ? rej(err) : res(success)));
 
 
-
 const strReplaceObj = {
 	"process.env.MONGO_USERNAME": "process.env.MONGODB_USERNAME",
 	"process.env.MONGO_PASSWORD": "process.env.MONGODB_PASSWORD",
@@ -39,7 +33,7 @@ const strReplaceObj = {
 const regInit = new RegExp(Object.keys(strReplaceObj).join("|"), "g");
 const dirCopy = {
 	"server/locales": "heroku/server/locales",
-	"server/public":	"heroku/server/public",
+	"server/public": "heroku/server/public",
 	"server/views": "heroku/server/views",
 	"server/bundle.js": "heroku/server/bundle.js"
 };
@@ -51,16 +45,15 @@ if (!Object.entries) {
 const locations = Object.entries(dirCopy);
 
 const app =
-	makeDir(__dirname + "/heroku", err => err)
-		.chain(content => makeDir(__dirname + "/heroku/server", err => err))
+	makeDir(__dirname + "/heroku")
+		.chain(content => makeDir(__dirname + "/heroku/server"))
 		.chain(content => readFile("config.js", "utf-8"))
 		.map(contents => contents.replace(regInit, matched => strReplaceObj[matched]))
 		.chain(contents => writeFile("heroku/config.js", contents))
 		.chain(contents => readFile("config.js", "utf-8"))
 		.map(contents => contents.replace("4", "9"))
 		.chain(contents => writeFile("heroku/server/config2.js", contents))
-		.map(content =>  locations.forEach(([origin,base])=> fs.copy(origin, base)));
-
+		.map(content => locations.forEach(([origin, base]) => fs.copy(origin, base)));
 
 app.fork(e => console.log(e),
 	x => console.log("success"));
